de2e135b75f4399dd69e734935f2e78dd88469da
nabeelrehman <pnabeel710@gmail.com>
2020-06-16 02:15:58 -0700

    its not completed

---
+++ app.js
@@ -0,0 +1,172 @@
+//Chapter No 1
+
+//Point 1
+
+//alert("Hello People Welcome To Our Web");
+
+//Point 2
+
+// alert("Error! Please enter a valid password");
+
+//Point 3
+
+// alert("Welcome to JS Land... \nHappy Coding!" )
+
+//Point 4
+
+// alert("Welcome to Js Land....");
+// alert("Prevent this page from additional dialogs")
+                      //END
+
+
+// Chapter No 2
+
+// Point 1
+
+// var username =""
+// alert(username)
+
+// Point 2
+
+// var myName ="Nabeel Rehman"
+// alert(myName)
+
+// Point 3
+
+// var message =""
+// message ="Hello World"
+// alert(message)
+
+// point 4 
+
+//  var username = prompt("Your Name")
+// alert (username)
+// var userage = prompt("Your age")
+// alert (userage)
+// var userqualification =prompt("Your qualification")
+// alert(userqualification)
+
+// point 5 
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// Point 6
+
+// var message ="My email address is"
+// var email ="pnabeel710@gmail.com"
+// alert(message+ " " +email)
+
+// Point 7
+
+// var Book
+// Book="A smarter way to learn javascript!"
+// alert (Book)
+
+// Point 8
+
+// document.write("Yah!..I can write HTML content through 
+
+// Point 9
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+                     //END
+
+
+
+// Chapter No 3
+
+// Point 1
+
+// var myage
+// myage="I am 17 year old"
+// alert(myage)
+// Point 3
+
+// var
+
+// Point 4
+
+// var visitorName
+// visitorName =prompt("Your Name")
+// var productName
+// productName =prompt("Product")
+// var quantity
+// quantity =prompt("Quantity")
+// document.write(visitorName)
+// document.write(" ordered")
+// document.write( quantity)
+// document.write( productName)
+// document.write(" on XYZ Clothing Store.")
+
+
+// chapter no 4
+
+// Point 1
+
+// var Name = "nabeel" , fatherName = "khalil" , age ="18"
+
+// Point 2
+
+// 5 legal variables
+// name,18age,$currency,_underscore,CamelCase
+// 5 illegal variables
+// name12,currency$,name_
+
+// Point 3
+
+// console.log 
+// <h1> Rules for naming JS variables </h1>
+// <p>Variable names can only contain letters, numbers,
+// dollar signs and underscores. </p>
+// <p>
+// Variables must begin with a name, dollar sign or
+// underscores.
+// </p>
+//  <p>Variable names are case sensitive</p> 
+// <p>Variable names should not be JS Keyword</p>
+
+
+
+// chapter no 5
+
+// point 1
+
+//  var a = 2 
+//  var b = a + 8
+// document.write("Sum of 2 and 8 is ", + " " + b )
+
+// Point 2
+
+// var a = 2 
+// var b = a * 8
+// document.write("multiplication of 2 and 8 is ", + " " + b )
+    
+// var a = 2 
+// var b = a - 8
+// document.write("subtraction of 2 and 8 is ", + " " + b )
+    
+// var a = 2 
+// var b = a / 8
+// document.write("division of 2 and 8 is ", + " " + b )
+    
+// Point 3
+
+//  var number = 48
+//  document.write("Value after variable declaration is ", + "   " + number)
+
+// var number = 44
+// document.write(number)
+
+// Point 4
+
+// var ticket = 700
+// var b = ticket * 5
+// document.write("Tottal cost to buy 5 tickets to a movie is " + b + "PKR")
+
+
+
+
+
+
+
+
